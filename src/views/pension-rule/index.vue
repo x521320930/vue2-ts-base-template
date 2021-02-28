55d68bde3ce7607e1f4c103195a8180f32656d8a
x521320930 <x521320930@gmail.com>
2021-02-28 15:52:22 +0800

    T: add

--- src/views/pension-rule/index.vue
+++ src/views/pension-rule/index.vue
@@ -3,23 +3,41 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { Button, Field, Icon, Picker } from 'vant'
+import { Button, Icon, Picker } from 'vant'
+import VPicker from '@/components/Picker/index.vue'
 @Component({
   name: 'PensionRule',
   components: {
     [Button.name]: Button,
-    [Field.name]: Field,
     [Icon.name]: Icon,
-    [Picker.name]: Picker
+    [Picker.name]: Picker,
+    VPicker
   }
 })
 export default class PensionRule extends Vue {
   // 提示弹框
-  public isShow = false
-  // 弹框标题与文本
-  public dialogTitle = ''
-  public dialogText = ''
-  public isCityPicker = false
+  public isPicker = false
+  public pickerColnums = ['2', '12', '6']
+
+  public queryData = this.$route.query
+  public picktext = '2'
+
+  // 首笔投资开启弹窗
+  public hanldeOpenPicker () {
+    this.isPicker = true
+  }
+
+  // 首笔投资确认弹窗
+  public hanldePickerConfirm (val: any) {
+    const { values, isShow } = val
+    this.picktext = values[0]
+    this.isPicker = isShow
+  }
+
+  // 首笔投资关闭弹窗
+  public hanldePickerCancel (val: boolean) {
+    this.isPicker = val
+  }
 }
 </script>
 <style lang="scss">
